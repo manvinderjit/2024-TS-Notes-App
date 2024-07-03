f99a8642eb17da28f6c406c8b907db9f11e410af
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2024-07-03 16:31:12 -0400

    setup: Project setup complete

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="justify-center text-center m-4"> 
+    <h1 class="text-3xl font-bold text-yellow-100 align-middle">Notes App</h1>
+    <div class="card">      
+    </div>    
+  </div>
+`;
